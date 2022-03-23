6b58acd0e37ae66e895460efca616b65fff51cff
Miguel López <lop18028@byui.edu>
2022-03-23 08:23:46 -0600

    Create and updated constants.py file.
    Create the new file and updated for Skeet Game.

---
+++ skeet/constants.py
@@ -0,0 +1,30 @@
+import arcade
+
+# These are Global constants to use throughout the game
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 500
+
+RIFLE_WIDTH = 100
+RIFLE_HEIGHT = 20
+RIFLE_COLOR = arcade.color.DARK_RED
+
+BULLET_RADIUS = 3
+BULLET_COLOR = arcade.color.BLACK_OLIVE
+BULLET_SPEED = 10
+
+TARGET_RADIUS = 20
+TARGET_COLOR = arcade.color.CARROT_ORANGE
+TARGET_SAFE_COLOR = arcade.color.AIR_FORCE_BLUE
+TARGET_SAFE_RADIUS = 15
+
+BONUS_TARGET_POINTS = 15
+SAFE_TARGET_POINTS = -10
+STANDART_TARGET_POINTS = 1
+
+RIFLE_IMAGE = r"skeet\resources\images/walle.png"
+BACKGROUND_IMAGE = r"skeet\resources\images/space-galaxy-background.jpg"
+STANDARD_TARGET_IMAGE = r"skeet\resources\images/standard.png"
+STRONG_TARGET_IMAGE = r"skeet\resources\images/strong.png"
+SAFE_TARGET_IMAGE = r"skeet\resources\images/eva.png"
+BONUS_TARGET_IMAGE = r"skeet\resources\images/bonus.png"
+BULLET_IMAGE = r"skeet\resources\images/bullet.png"
